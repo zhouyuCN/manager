65b1a82c34f74c89cd4fc59247bb769768c75d34
zhouyuCN <928017614@qq.com>
2019-03-30 12:55:28 +0800

    登入界面初步搭建

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,17 +1,50 @@
 <template>
-    <div class="login">登入</div>
+  <div class="login">
+    <div class="login-box">
+      <h2>用户登入</h2>
+      <el-form label-position="top" label-width="80px" >
+        <el-form-item ><span class="ico">* </span><span> 用户名称</span>
+          <el-input ></el-input>
+        </el-form-item>
+         <el-form-item ><span class="ico">* </span><span> 用户密码</span>
+          <el-input></el-input>
+        </el-form-item>
+        <el-form-item >
+          <el-button class="btn" type="primary">登入</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-name:'login'
-}
+  name: "login"
+};
 </script>
 
 <style lang="scss">
 .login {
-    width: 100px;
-    height: 100px;
-    background-color: red;
+    height: 100%;
+    background-color:#324152 ;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .login-box{
+        width: 540px;
+        height: 420px;
+        box-sizing: border-box;
+        border: 1px solid black;
+        border-radius: 10px;
+        background-color: #fff;
+        padding: 35px;
+        .btn {
+            width: 100%;
+        }
+        .ico {
+            display: inline-block;
+            color: red;
+        }
+    }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+//导入样式
+import './assets/base.scss'
 //饿了么 UI
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
